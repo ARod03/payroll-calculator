232b47761091e74e519ed5db53e522e34a258b6d
adrianrodriguez <adrian2003@gmail.com>
2026-04-09 15:46:23 -0500

    Finished exercise 4

---
+++ src/main/java/com/pluralsight/Payroll.java
@@ -0,0 +1,33 @@
+package com.pluralsight;
+
+import java.util.Scanner;
+
+public class Payroll {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        //Get the name of the person
+        System.out.println("Please enter the name of employee: ");
+        String name = scanner.nextLine();
+
+        //Get the hours worked
+        System.out.println("Enter the hours worked per week of employee: ");
+        double hours = scanner.nextDouble();
+
+        //Get the pay rate of the employee
+        System.out.println("Enter the pay rate of employee: ");
+        double payRate = scanner.nextDouble();
+
+        //Calculate the pay role of employee
+        double payRoll = payRate * hours * 52;
+
+        //Display the full payroll with employee
+
+        System.out.println("The employee, " + name + ", earns $" + payRate + " per hour and works "
+                + hours + " per week, making their salary $" + payRoll + " per year.");
+
+
+    }
+}
