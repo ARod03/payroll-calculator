9c7a088f9ce2c3fa3477e1256e95abd5ffa6a3c8
adrianrodriguez <adrian2003@gmail.com>
2026-04-10 09:34:54 -0500

    Finished the optional exercise

--- src/main/java/com/pluralsight/Payroll.java
+++ src/main/java/com/pluralsight/Payroll.java
@@ -1,6 +1,8 @@
 package com.pluralsight;
 
+import java.util.Objects;
 import java.util.Scanner;
+
 
 public class Payroll {
 
@@ -25,8 +27,25 @@ public class Payroll {
 
         //Display the full payroll with employee
 
-        System.out.println("The employee, " + name + ", earns $" + payRate + " per hour and works "
-                + hours + " per week, making gross pay $" + grossPay + " week.");
+        //System.out.println("The employee, " + name + ", earns $" + payRate + " per hour and works "
+                //+ hours + " per week, making gross pay $" + grossPay + " week.");
+
+        //Optional if statement exercise
+
+        //Get a value for overtime
+        double overTimePay = (hours - 40) * (payRate * 1.5);
+
+        //Create an if and else statement whether if the hours worked is 40 or less
+
+        //This display if the employee has worked overtime
+        if(hours > 40) {
+            System.out.println("The employee, " + name + ", earns $" + payRate + " per hour and works "
+                + hours + " per week, making gross pay $" + (overTimePay + grossPay) +  " week with overtime.");}
+
+        //This displays if the employee has worked 40 hours or less
+        else {
+            System.out.println("The employee, " + name + ", earns $" + payRate + " per hour and works "
+                + hours + " per week, making gross pay $" + grossPay + " week.");}
 
 
 
